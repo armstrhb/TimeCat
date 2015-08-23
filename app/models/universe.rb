a38750e5ce1f1ac6e84ae2a9f6b1dc4ee6054d1e
armstrhb <henry.b.armstrong@gmail.com>
2015-08-23 11:33:05 -0400

    universe validations, unit tests

--- app/models/universe.rb
+++ app/models/universe.rb
@@ -1,4 +1,7 @@
 class Universe < ActiveRecord::Base
   has_many :time_instants
   has_one :time_grouping, foreign_key: "universe_id"
+
+  validates :name, presence: true, uniqueness: true, length: { in: 2..100 }
+  validates :description, length: { maximum: 255 }
 end
--- test/models/universe_test.rb
+++ test/models/universe_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UniverseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "universe name must be unique" do
+    uni = universes(:lotr)
+    assert_not Universe.new(:name => uni.name).save, "universe saved with duplicate name"
+    assert Universe.new(:name => uni.name + "1234").save, "universe failed to save with unique name"
+  end
+
+  test "universe name must be present" do
+    assert_not Universe.new.save, "universe saved with nil name"
+    assert_not Universe.new(name: "").save, "universe saved with empty name"
+    assert Universe.new(name: "blueberry cobbler").save, "universe failed to save with name"
+  end
+
+  test "description must be less than 255 characters" do
+    assert Universe.new(name: "test_uni1", description: "k").save, "universe save failed with 1 length description"
+    assert Universe.new(name: "test_uni2", description: ("k" * 255)).save, "universe save failed with 255 length description"
+    assert_not Universe.new(name: "test_uni3", description: ("k" * 256)).save, "universe saved with long description"
+  end
+
+  test "description allowed to be empty" do
+    assert Universe.new(name: "test_uni1").save, "universe failed to save with nil description"
+    assert Universe.new(name: "test_uni2", description: "").save, "universe failed to save with empty description"
+  end
 end
