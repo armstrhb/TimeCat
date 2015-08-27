f1739f71bc9d97c0e232564a203f2ea511dab91d
armstrhb <henry.b.armstrong@gmail.com>
2015-08-27 19:45:32 -0400

    Events part_of, people, property, fixtures

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,9 @@
 class Event < ActiveRecord::Base
   belongs_to :time_instant, foreign_key: 'time_instant_id'
   belongs_to :universe, foreign_key: 'universe_id'
+  belongs_to :part_of, class_name: 'Event', foreign_key: 'part_of_id'
   has_and_belongs_to_many :locations, join_table: 'event_locations', uniq: true
+  has_and_belongs_to_many :people, join_table: 'event_people', uniq: true
 
   validates :name, presence: true, length: { in: 2..255 }
   validates :description, length: { maximum: 255 }
