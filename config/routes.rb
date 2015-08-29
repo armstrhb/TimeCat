8de97bfd4adc62fec3a0a46776ba89af01cd6d3c
armstrhb <henry.b.armstrong@gmail.com>
2015-08-28 23:05:26 -0400

    index view, show base

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get '/timelines', to: 'timelines#index'
-  get '/timelines/:name', to: 'timelines#show'
+  get '/timelines', to: 'timelines#index', as: :timelines
+  get '/timelines/:name', to: 'timelines#show', as: :timeline_detail
 end
