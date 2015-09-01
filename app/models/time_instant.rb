09401ba05592ddfe85aa8c71f9a0ae26ff26e604
armstrhb <henry.b.armstrong@gmail.com>
2015-08-31 22:27:50 -0400

    time_instant to_i method, for sorting/ordering

--- app/models/time_instant.rb
+++ app/models/time_instant.rb
@@ -9,4 +9,24 @@ class TimeInstant < ActiveRecord::Base
   def time_grouping
     universe.time_grouping
   end
+
+  def to_i
+    val = era.sequence.to_s
+
+    val += format_slot(slot_1)
+    val += format_slot(slot_2)
+    val += format_slot(slot_3)
+    val += format_slot(slot_4)
+    val += format_slot(slot_5)
+    val += format_slot(slot_6)
+    val += format_slot(slot_7)
+    val += format_slot(slot_8)
+
+    val.to_i
+  end
+
+  private
+    def format_slot(slot)
+      sprintf("%012d", (slot == nil ? 0 : slot))
+    end
 end
