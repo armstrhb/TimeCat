c399ff3ff7658a7188e4420aeafbec985d5bbcc5
armstrhb <henry.b.armstrong@gmail.com>
2015-09-03 22:35:43 -0400

    timeline show work
    need for time instant format recognized

--- app/helpers/timelines_helper.rb
+++ app/helpers/timelines_helper.rb
@@ -3,4 +3,7 @@ module TimelinesHelper
     return events if events == nil or events.length == 0
     events.sort_by { |e| e.time_instant.to_i }
   end
+
+  def format_time_instant(time_instant)
+  end
 end
