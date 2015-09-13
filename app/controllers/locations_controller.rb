bbc1cbdc2b5317a9621bef864c42142e09898569
armstrhb <henry.b.armstrong@gmail.com>
2015-09-12 22:06:30 -0400

    Locations index work

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,14 @@
 class LocationsController < ApplicationController
+  def index
+    @universe = Universe.find_by(name: params[:name])
+    @locations = @universe.locations.where(part_of: nil)
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  def search
+    @locations = Location.where(Location.arel_table[:name].matches("%#{params[:name]}%"))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'universes#index'
 
   get '/:name', to: 'universes#show', as: :universe_detail
-  get '/:name/places', to: 'places#index', as: :places_index
+  get '/:name/locations', to: 'locations#index', as: :locations_index
   get '/:name/people', to: 'people#index', as: :people_index
   get '/:name/description', to: 'universes#description', as: :universe_description
   post '/:name/description', to: 'universes#update_description', as: :universe_update_description
