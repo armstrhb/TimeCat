a05d649dd6a6c111af7d331580af369a1c1d3883
armstrhb <henry.b.armstrong@gmail.com>
2015-10-09 22:43:29 -0400

    event locations/people sorted by name

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,8 @@ class Event < ActiveRecord::Base
   belongs_to :end_time_instant, class_name: 'TimeInstant', foreign_key: 'end_time_instant_id'
   belongs_to :universe, foreign_key: 'universe_id'
   belongs_to :part_of, class_name: 'Event', foreign_key: 'part_of_id'
-  has_and_belongs_to_many :locations, join_table: 'event_locations', uniq: true
-  has_and_belongs_to_many :people, join_table: 'event_people', uniq: true
+  has_and_belongs_to_many :locations, -> { order(name: :asc) }, join_table: 'event_locations', uniq: true
+  has_and_belongs_to_many :people, -> { order(name: :asc) }, join_table: 'event_people', uniq: true
 
   validates :name, presence: true, length: { in: 2..255 }
   validates :description, length: { maximum: 65536 }
